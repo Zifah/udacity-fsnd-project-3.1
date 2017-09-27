7b32531c8d7bff87017a1c3f43970345351898aa
Hafiz Adewuyi <hafiz@chatdesk.com>
2017-09-27 03:21:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Udacity Full Stack Web Developer Nanodegree Project 3.1 (Movie trailer)
+This program answers some analytical questions about a theoretical news website by running SQL queries against the website database.
+
+### Questions answered
+- What are the most popular three articles of all time?
+- Who are the most popular article authors of all time?
+- On which days did more than 1% of requests lead to errors?
+
+### Running the program
+1. Logon to your Linux machine which holds the PGSQL database news (populated from newsdata.sql) via the CLI
+2. Ensure that *Python 2.7* or higher is installed on the machine
+3. Ensure that *psycopg2* Python package is installed
+4. Copy *news_reports.py* from this project to your machine 
+5. Set your CLI working directory to the one containing *news_reports.py*
+5. Run command "python news_reports.py". If everything is set up correctly, the program will print out the answer to each question sequentially, in the same order as listed here (see sample-output.txt for a sample of what to expect)
---
+++ news_reports.py
@@ -0,0 +1,89 @@
+# Python 2.7.12 or Higher
+import psycopg2
+
+DBNAME = "news"
+
+POPULAR_ARTICLES_QUERY = """
+select a.title, count(l.id) as views
+from articles a left join log l
+on concat('/article/',a.slug) = l.path
+group by a.title 
+order by views desc
+limit 3;
+"""
+AUTHOR_RANKING_QUERY = """
+select au.name, count(l.id) as views
+from authors au
+left join articles ar on ar.author = au.id
+left join log l on concat('/article/',ar.slug) = l.path
+group by au.id
+order by views desc;
+"""
+ERRONEOUS_DAY_QUERY = """
+select to_char(error_summary.date, 'Mon DD, YYYY') date, 
+round((error_size::float * 100/all_size)::numeric, 2) percent 
+
+from (select time::date as date, count(id) error_size
+    from log 
+    where status != '200 OK'
+    group by date) as error_summary
+
+left join (select time::date date, count(id) all_size 
+from log 
+group by date) as all_summary
+
+on error_summary.date = all_summary.date
+
+where error_size::float * 100/all_size > 1;
+"""
+
+
+def show_popular_articles():
+    """Print out the top 3 articles by popularity among website visitors"""
+    cursor = DB_CONNECTION.cursor()
+    cursor.execute(POPULAR_ARTICLES_QUERY)
+    results = cursor.fetchall()
+
+    print("POPULAR ARTICLES")
+    print("-------------------------------")
+    for title, views in results:
+        print("%s - %s views" % (title, views))
+    print("\r\n")
+
+
+def show_authors_by_popularity():
+    """List out all authors based on popularity
+    (how many visits their articles have cummulatively)"""
+    cursor = DB_CONNECTION.cursor()
+    cursor.execute(AUTHOR_RANKING_QUERY)
+    results = cursor.fetchall()
+
+    print("POPULAR AUTHORS")
+    print("-------------------------------")
+    for name, views in results:
+        print("%s - %s views" % (name, views))
+    print("\r\n")
+
+
+def show_high_error_days():
+    """Print out any days where HTTP error responses
+    exceed 1% of all requests, along with the percentage"""
+    cursor = DB_CONNECTION.cursor()
+    cursor.execute(ERRONEOUS_DAY_QUERY)
+    results = cursor.fetchall()
+
+
+    print("HIGH ERROR DAYS")
+    print("-------------------------------")
+    for date, percent in results:
+        print("%s - %s%% errors" % (date, percent))
+    print("\r\n")
+
+
+if __name__ == '__main__':
+    """Print out all the statistics"""
+    DB_CONNECTION = psycopg2.connect(database=DBNAME)
+    show_popular_articles()
+    show_authors_by_popularity()
+    show_high_error_days()
+    DB_CONNECTION.close()
